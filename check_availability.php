06bfa43ed3f1f56a1c9ea720a8bda0209db77734
Mahfujur51 <overcastmoon@gmail.com>
2020-04-28 14:36:03 +0600

    Registration complete with validation

---
+++ check_availability.php
@@ -0,0 +1,28 @@
+<?php 
+require_once("dbconnection.php");
+// code user email availablity
+if(!empty($_POST["email"])) {
+	$email= $_POST["email"];
+	if (filter_var($email, FILTER_VALIDATE_EMAIL)===false) {
+
+		echo "error : You did not enter a valid email.";
+	}
+	else {
+		$sql ="SELECT * FROM tbl_user WHERE email='$email'";
+		$query=mysqli_query($con,$sql);
+		$num=mysqli_num_rows($query);
+		if ($num>0) {
+			echo "<span style='color:red'> Email already exists .</span>";
+			echo "<script>$('#submit').prop('disabled',true);</script>";
+			# code...
+		}
+		 else{
+
+			echo "<span style='color:green'> Email available for Registration .</span>";
+			echo "<script>$('#submit').prop('disabled',false);</script>";
+		}
+	}
+}
+
+
+?>
--- dbconnection.php
+++ dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost", "root", "", "crm");
+$con=mysqli_connect("localhost", "root", "", "crm_online");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
