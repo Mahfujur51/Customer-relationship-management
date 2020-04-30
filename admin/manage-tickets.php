205037087f09b9e949fe76d62ca4a78a10612eef
Mahfujur51 <overcastmoon@gmail.com>
2020-04-30 18:01:56 +0600

    done with all dynamic

--- admin/manage-tickets.php
+++ admin/manage-tickets.php
@@ -7,84 +7,91 @@ include("checklogin.php");
 check_login();
 if(isset($_POST['update']))
 {
-$adminremark=$_POST['aremark'];
-$fid=$_POST['frm_id'] ;
-mysqli_query($con,"update ticket set admin_remark='$adminremark' where id='$fid'");
-echo '<script>alert("Ticket has been updated.")</script>';
+  $admin_remark=$_POST['admin_remark'];
+  $id=$_POST['frm_id'] ;
+  $upsql="UPDATE tbl_ticket SET admin_remark='$admin_remark' WHERE id='$id'";
+  $upquery=mysqli_query($con,$upsql);
+  if ($upquery) {
+    echo "<script>alert('Ticket has been update!!')</script>";
+  }
 }
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
-<meta charset="utf-8" />
-<title>User | Ticket Support</title>
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-<meta content="" name="description" />
-<meta content="" name="author" />
-<link href="assets/plugins/pace/pace-theme-flash.css" rel="stylesheet" type="text/css" media="screen"/>
-<link href="assets/plugins/bootstrap-wysihtml5/bootstrap-wysihtml5.css" rel="stylesheet" type="text/css"/>
-<link href="assets/plugins/boostrapv3/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-<link href="assets/plugins/boostrapv3/css/bootstrap-theme.min.css" rel="stylesheet" type="text/css"/>
-<link href="assets/plugins/font-awesome/css/font-awesome.css" rel="stylesheet" type="text/css"/>
-<link href="assets/css/animate.min.css" rel="stylesheet" type="text/css"/>
-<link href="assets/plugins/jquery-scrollbar/jquery.scrollbar.css" rel="stylesheet" type="text/css"/>
-<link href="assets/css/style.css" rel="stylesheet" type="text/css"/>
-<link href="assets/css/responsive.css" rel="stylesheet" type="text/css"/>
-<link href="assets/css/custom-icon-set.css" rel="stylesheet" type="text/css"/>
+  <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+  <meta charset="utf-8" />
+  <title>User | Ticket Support</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+  <meta content="" name="description" />
+  <meta content="" name="author" />
+  <link href="assets/plugins/pace/pace-theme-flash.css" rel="stylesheet" type="text/css" media="screen"/>
+  <link href="assets/plugins/bootstrap-wysihtml5/bootstrap-wysihtml5.css" rel="stylesheet" type="text/css"/>
+  <link href="assets/plugins/boostrapv3/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+  <link href="assets/plugins/boostrapv3/css/bootstrap-theme.min.css" rel="stylesheet" type="text/css"/>
+  <link href="assets/plugins/font-awesome/css/font-awesome.css" rel="stylesheet" type="text/css"/>
+  <link href="assets/css/animate.min.css" rel="stylesheet" type="text/css"/>
+  <link href="assets/plugins/jquery-scrollbar/jquery.scrollbar.css" rel="stylesheet" type="text/css"/>
+  <link href="assets/css/style.css" rel="stylesheet" type="text/css"/>
+  <link href="assets/css/responsive.css" rel="stylesheet" type="text/css"/>
+  <link href="assets/css/custom-icon-set.css" rel="stylesheet" type="text/css"/>
 </head>
 <body class="">
-<?php include("header.php");?>
-<div class="page-container row"> 
-  
-      <?php include("leftbar.php");?>
+  <?php include("header.php");?>
+  <div class="page-container row"> 
     
-      <div class="clearfix"></div>
-    </div>
+    <?php include("leftbar.php");?>
+    
+    <div class="clearfix"></div>
   </div>
-  <div class="page-content">
-    <div id="portlet-config" class="modal hide">
-      <div class="modal-header">
-        <button data-dismiss="modal" class="close" type="button"></button>
-        <h3>Widget Settings</h3>
-      </div>
-      <div class="modal-body"> Widget settings form goes here </div>
+</div>
+<div class="page-content">
+  <div id="portlet-config" class="modal hide">
+    <div class="modal-header">
+      <button data-dismiss="modal" class="close" type="button"></button>
+      <h3>Widget Settings</h3>
+    </div>
+    <div class="modal-body"> Widget settings form goes here </div>
+  </div>
+  <div class="clearfix"></div>
+  <div class="content">
+    <ul class="breadcrumb">
+      <li>
+        <p>Home</p> 
+      </li>
+      <li><a href="#" class="active">View Ticket</a></li>
+    </ul>
+    <div class="page-title"> <i class="icon-custom-left"></i>
+      <h3>Ticket Support</h3>
     </div>
     <div class="clearfix"></div>
-    <div class="content">
-      <ul class="breadcrumb">
-        <li>
-          <p>Home</p> 
-        </li>
-        <li><a href="#" class="active">View Ticket</a></li>
-      </ul>
-      <div class="page-title"> <i class="icon-custom-left"></i>
-        <h3>Ticket Support</h3>
-      </div>
-      <div class="clearfix"></div>
-      
-      <h4> <span class="semi-bold">Tickets</span></h4>
-      <br>
-     <?php $rt=mysqli_query($con,"select * from ticket order by id desc");
-													while($row=mysqli_fetch_array($rt))
-													{
-													?> 
+    
+    <h4> <span class="semi-bold">Tickets</span></h4>
+    <br>
+    <?php
+    $sql="SELECT * FROM tbl_ticket";
+    $query=mysqli_query($con,$sql);
+    $num=mysqli_num_rows($query);
+
+    if ($num>0) {
+     while ( $result=mysqli_fetch_array($query)) {
+      ?> 
       <div class="row">
         <div class="col-md-12">
           <div class="grid simple no-border">
             <div class="grid-title no-border descriptive clickable">
-              <h4 class="semi-bold"><?php echo $row['subject'];?></h4>
-              <p ><span class="text-success bold">Ticket #<?php echo $_SESSION['sid']=$row['ticket_id'];?></span> - Created on <?php echo $row['posting_date'];?>
-             <span class="label label-important"><?php echo $row['status'];?></span></p>
+              <h4 class="semi-bold"><?php echo $result['subject'];?></h4>
+              <p ><span class="text-success bold">Ticket #<?php echo $result['ticket_id'];?></span> - Created on <?php echo $result['postig_date'];?>
+              <span class="label label-important"><?php echo $result['status'];?></span></p>
               <div class="actions"> <a class="view" href="javascript:;"><i class="fa fa-search"></i></a>  </div>
             </div>
             <div class="grid-body  no-border" style="display:none">
               <div class="post">
                 <div class="user-profile-pic-wrapper">
-                  <div class="user-profile-pic-normal"> <img width="35" height="35" data-src-retina="userimages/<?php echo $row['user_image'];?>" data-src="userimages/<?php echo $row['user_image'];?>" src="userimages/<?php echo $row['user_image'];?>" alt=""> </div>
+                  <div class="user-profile-pic-normal"> <img width="35" height="35" data-src-retina="../userimages/user.jpg" data-src="../userimages/user.jpg" src="../userimages/user.jpg" alt=""> </div>
                 </div>
                 <div class="info-wrapper">
-                  <div class="info"><?php echo $row['ticket'];?> </div>
+                  <div class="info"><?php echo $result['ticket'];?> </div>
                   <div class="clearfix"></div>
                 </div>
                 <div class="clearfix"></div>
@@ -93,43 +100,44 @@ echo '<script>alert("Ticket has been updated.")</script>';
               <div class="form-actions">
                 <div class="post col-md-12">
                   <div class="user-profile-pic-wrapper">
-                    <div class="user-profile-pic-normal"> <img width="35" height="35" data-src-retina="images/admin.ico" data-src="images/admin.ico" src="images/admin.ico" alt=""> </div>
+                    <div class="user-profile-pic-normal"> <img width="35" height="35" data-src-retina="../userimages/user.jpg" data-src="../userimages/user.jpg" src="../userimages/user.jpg" alt=""> </div>
                   </div>
                   <div class="info-wrapper">
- <form name="adminr"  method="post" enctype="multipart/form-data">
-                      <br>
-                     <textarea name="aremark" cols="50" rows="4" required="true"><?php echo $row['admin_remark'];?></textarea>
-                      <hr>
-                      <p class="small-text"> 
-                        <input name="update" type="submit"  class="txtbox1" id="Update" value="update" size="40" />
- <input name="frm_id" type="hidden" id="frm_id" value="<?php echo $row['id'];?>" /></p>
-                      </form>
-                    </div>
-                    <div class="clearfix"></div>
+                   <form  method="post"  action="">
+                    <br>
+                    <textarea name="admin_remark" cols="50" rows="4" required="true"><?php echo $result['admin_remark'];?></textarea>
+                    <hr>
+                    <p class="small-text"> 
+                      <input name="update" type="submit"  class="txtbox1" id="Update" value="update" size="40" />
+                      <input name="frm_id" type="hidden" id="frm_id" value="<?php echo $result['id'];?>" /></p>
+                    </form>
                   </div>
                   <div class="clearfix"></div>
                 </div>
+                <div class="clearfix"></div>
               </div>
             </div>
-               <?php } ?>
           </div>
-        </div>
-      
-       
-          </div>
+          
         </div>
       </div>
+      <?php  
+    }      } ?>
     
-        
-          </div>
-        </div>
-      </div>
-      
-          </div>
-        </div>
-      </div>
-    </div>
   </div>
+</div>
+</div>
+
+
+</div>
+</div>
+</div>
+
+</div>
+</div>
+</div>
+</div>
+</div>
 
 </div>
 <!-- END CONTAINER -->
